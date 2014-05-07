6cd7319f8dcab85ec9b4c516baae5020bb893536
mfcr <manuel_campelo@yahoo.com>
2014-05-08 00:47:10 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,24 @@
+#CODE THAT IS REPEATED ACROSS AL FILES OF ASSIGMENT
+# GET data.
+data<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
+#format dates
+data[,1]<-as.Date(data[,1],"%d/%m/%Y")
+
+# Select data where dates are only 2007-02-01 and 2007-02-02,
+data<-rbind(data[as.Date(data[,1])=="2007-02-01",],data[as.Date(data[,1])=="2007-02-02",])
+
+# Join date and time and bind it to the existing data.
+data<-cbind(strptime(paste(data[,1],data[,2]), "%Y-%m-%d %H:%M:%S"),data[,c(3:9)]) 
+#END OF REPEATED CODE
+
+# Open "png" graphic device and name the output file as "plot1.png".
+png(file="plot1.png")
+
+# Setting parameter of backgroung as transparent.
+par(bg="transparent")
+
+# Plot histogram.
+hist(data$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+
+# Don't forget to turn off the graphic device.
+dev.off()
